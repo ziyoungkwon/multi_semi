0e906ba1344b1a1988ebf8e56e1678bdeef7b1f0
ziyoungkwon <gy011003@naver.com>
2025-11-05 16:03:25 +0900

    chore : 기본 프로젝트 셋팅

---
+++ src/main/java/com/multi/multi_semi/MultiSemiApplication.java
@@ -0,0 +1,13 @@
+package com.multi.multi_semi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MultiSemiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MultiSemiApplication.class, args);
+    }
+
+}
