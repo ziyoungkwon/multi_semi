1805a93ec0a9559c00c869bac700d2fd1616b97d
oJWJo <terry99025@gmail.com>
2025-11-07 13:44:56 +0900

    push test jw1
    push test 1

--- src/main/java/com/multi/multi_semi/MultiSemiApplication.java
+++ src/main/java/com/multi/multi_semi/MultiSemiApplication.java
@@ -5,9 +5,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class MultiSemiApplication {
-
     public static void main(String[] args) {
+        System.out.println("Hello World");
         SpringApplication.run(MultiSemiApplication.class, args);
     }
-
 }
