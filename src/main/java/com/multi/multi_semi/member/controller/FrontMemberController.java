2c286725febdc9741f7fbee53142d9dc1287bd25
Lee Ji Heon <jiheon340@gmail.com>
2025-11-12 16:19:09 +0900

    [feature] mem api경로에서 {email} 제거. 회원가입 시 주소, 자기소개, 휴대폰번호도 들어가도록 수정.

--- src/main/java/com/multi/multi_semi/member/controller/FrontMemberController.java
+++ src/main/java/com/multi/multi_semi/member/controller/FrontMemberController.java
@@ -1,12 +1,16 @@
 package com.multi.multi_semi.member.controller;
 
+import com.multi.multi_semi.auth.dto.CustomUser;
 import com.multi.multi_semi.member.dto.MemberDto;
+import com.multi.multi_semi.member.dto.req.UpdateMemberReqDto;
 import com.multi.multi_semi.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -16,8 +20,9 @@ public class FrontMemberController {
 
     private final MemberService memberService;
 
-    @GetMapping("/members/{email}/edit-info")
-    public String updateMemberInfo(Model model, @PathVariable("email") String email) {
+    @GetMapping("/members/edit-info")
+    public String updateMemberInfo(Model model, @AuthenticationPrincipal CustomUser customUser) {
+        String email = customUser.getEmail();
         MemberDto memberDto = memberService.findMemberByEmail(email).get();
         model.addAttribute("member", memberDto);
         model.addAttribute("currentEmail", memberDto.getEmail());
@@ -25,13 +30,25 @@ public class FrontMemberController {
     }
 
 
-    @GetMapping("/members/{email}/edit-pwd")
-    public String updateMemberPwd(Model model, @PathVariable("email") String email) {
+    @GetMapping("/members/edit-pwd")
+    public String updateMemberPwd(Model model, @AuthenticationPrincipal CustomUser customUser) {
+        String email = customUser.getEmail();
         MemberDto memberDto = memberService.findMemberByEmail(email).get();
         model.addAttribute("member", memberDto);
         return "member/edit-pwd";
     }
 
+
+    @GetMapping("/admin/members/{email}/edit-info")
+    public String adminUpdateMemberInfo(Model model , @PathVariable("email") String email, @RequestBody UpdateMemberReqDto updateMemberReqDto){
+
+        MemberDto memberDto = memberService.findMemberByEmail(email).get();
+
+        model.addAttribute("member", memberDto);
+        model.addAttribute("currentEmail", memberDto.getEmail());
+
+        return "/admin/member/edit-info";
+    }
 
 
 
--- src/main/java/com/multi/multi_semi/member/controller/MemberController.java
+++ src/main/java/com/multi/multi_semi/member/controller/MemberController.java
@@ -1,6 +1,7 @@
 package com.multi.multi_semi.member.controller;
 
 
+import com.multi.multi_semi.auth.dto.CustomUser;
 import com.multi.multi_semi.common.ResponseDto;
 import com.multi.multi_semi.member.dto.MemberDto;
 import com.multi.multi_semi.member.dto.req.UpdateMemberReqDto;
@@ -8,6 +9,7 @@ import com.multi.multi_semi.member.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Optional;
@@ -31,8 +33,10 @@ public class MemberController {
     }
 
 
-    @GetMapping("/members/{email}")
-    public ResponseEntity<ResponseDto> findMemberByEmail(@PathVariable("email") String email){
+    @GetMapping("/members")
+    public ResponseEntity<ResponseDto> findMemberByEmail(@AuthenticationPrincipal CustomUser customUser){
+        String email = customUser.getEmail();
+
         Optional<MemberDto> member = memberService.findMemberByEmail(email);
 
         if(member.isEmpty()){
@@ -45,23 +49,29 @@ public class MemberController {
 
 
 
-    @PatchMapping("/members/{email}/edit-info")
-    public ResponseEntity<ResponseDto> updateMemberInfo(@PathVariable("email") String email, @RequestBody UpdateMemberReqDto updateMemberReqDto){
+    @PatchMapping("/members/edit-info")
+    public ResponseEntity<ResponseDto> updateMemberInfo(@AuthenticationPrincipal CustomUser customUser, @RequestBody UpdateMemberReqDto updateMemberReqDto){
+        String email = customUser.getEmail();
+
         memberService.updateMemberInfo(email, updateMemberReqDto);
 
         return ResponseEntity.ok(new ResponseDto(HttpStatus.OK, "회원정보 수정 성공", null));
     }
 
 
-    @PatchMapping("/members/{email}/edit-pwd")
-    public ResponseEntity<ResponseDto> updateMemberPwd(@PathVariable("email") String email, @RequestBody UpdateMemberReqDto updateMemberReqDto){
+    @PatchMapping("/members/edit-pwd")
+    public ResponseEntity<ResponseDto> updateMemberPwd(@AuthenticationPrincipal CustomUser customUser, @RequestBody UpdateMemberReqDto updateMemberReqDto){
+        String email = customUser.getEmail();
+
         memberService.updateMemberPwd(email, updateMemberReqDto);
 
         return ResponseEntity.ok(new ResponseDto(HttpStatus.OK, "회원비밀번호 수정 성공", null));
     }
 
-    @DeleteMapping("/members/{email}")
-    public ResponseEntity<ResponseDto> deleteMember(@PathVariable("email") String email){
+    @DeleteMapping("/members")
+    public ResponseEntity<ResponseDto> deleteMember(@AuthenticationPrincipal CustomUser customUser){
+        String email = customUser.getEmail();
+
         memberService.deleteMemberByEmail(email);
 
         return ResponseEntity.ok(new ResponseDto(HttpStatus.OK, "회원 탈퇴 성공", null));
@@ -71,6 +81,7 @@ public class MemberController {
 
     @PatchMapping("/admin/members/{email}/edit-info")
     public ResponseEntity<ResponseDto> adminUpdateMemberInfo(@PathVariable("email") String email, @RequestBody UpdateMemberReqDto updateMemberReqDto){
+
         memberService.updateMemberInfo(email, updateMemberReqDto);
 
         return ResponseEntity.ok(new ResponseDto(HttpStatus.OK, "회원정보 수정 성공(관리자)", null));
